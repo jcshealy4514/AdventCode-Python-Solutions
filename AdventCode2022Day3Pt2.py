312e3adecb04f413e5b701b0dfde37b29728261a
jcshealy4514 <65636255+jcshealy4514@users.noreply.github.com>
2022-12-04 12:48:11 -0500

    Added Day 3 Pt 2 for advent code

--- AdventCode2022Day3Pt2.py
+++ AdventCode2022Day3Pt2.py
@@ -1,6 +1,8 @@
 import string ##to initialize ascii lowercase and uppercase strings in one go
 import os ##to pull windows environment variable - USERPROFILE
 TotalRuckSackNumber = 0
+RuckSacksChecked = 0
+Alphabet = list(string.ascii_letters)
 
 #Calling same script to remove missing characters from both compartments
 def removeMissingChar(compartment):
@@ -13,22 +15,18 @@ def removeMissingChar(compartment):
 
 with open(os.environ['USERPROFILE'] + '/Downloads/AdventcodeDay3Input.txt') as file:
     for line in file.readlines():
-            
-        #Initilize Alphabet
-        Alphabet = list(string.ascii_letters)
 
-        #To select first and second half of string (len is weird? Both compartments are not same length, this seems like a broken challenge)
-        firstCompartment = (len(line) // 2)
-        secondCompartment = len(line)
-
-        removeMissingChar(line[0:firstCompartment])
-        removeMissingChar(line[firstCompartment:secondCompartment])
+        removeMissingChar(line)
+        RuckSacksChecked += 1
 
         #Using ord method to convert letter to number. Lowercase and uppercase numbers start at different numbers so I substract to get them to the numbers befitting the challenge.
-        letter = Alphabet[0]
-        if(letter.isupper()):
-            TotalRuckSackNumber += (ord(letter) - 38)
-        else:
-            TotalRuckSackNumber += (ord(letter) - 96)
+        if(RuckSacksChecked == 3):
+            letter = Alphabet[0]
+            if(letter.isupper()):
+                TotalRuckSackNumber += (ord(letter) - 38)
+            else:
+                TotalRuckSackNumber += (ord(letter) - 96)
+            RuckSacksChecked = 0
+            Alphabet = list(string.ascii_letters)
     
 print(TotalRuckSackNumber)
