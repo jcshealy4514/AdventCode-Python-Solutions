808186e79605ebbcc39dac45c99de4afeb1a0628
jcshealy4514 <65636255+jcshealy4514@users.noreply.github.com>
2022-12-06 22:27:00 -0500

    Advent Code Day 6

---
+++ AdventCode2022Day6Pt2.py
@@ -0,0 +1,24 @@
+import os ##to pull windows environment variable - USERPROFILE
+import time
+
+with open(os.environ['USERPROFILE'] + '/Downloads/Day6.txt') as file:
+    for line in file.readlines():
+        charStart = 0
+        charEnd = 14
+
+        while charEnd <= len(line):
+            charString = line[charStart:charEnd]
+            charArray = []
+
+            for char in charString:
+                if char not in charArray:
+                    charArray.append(char)
+            
+            if len(charArray) == 14:
+                print(charEnd)
+                quit()
+            
+            charStart += 1
+            charEnd += 1
+            
+
