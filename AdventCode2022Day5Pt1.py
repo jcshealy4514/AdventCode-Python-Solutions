56b3c373b8aff8f18835be6f954056eefc8bee8e
jcshealy4514 <65636255+jcshealy4514@users.noreply.github.com>
2022-12-06 21:01:13 -0500

    Create AdventCode2022Day5Pt1.py

---
+++ AdventCode2022Day5Pt1.py
@@ -0,0 +1,76 @@
+import os ##to pull windows environment variable - USERPROFILE
+
+#This entire definition will remain here as a tribute to the sheer magnitude of dumbassery one can achieve in the pursuit of.. anything really.
+def initalizeTable(file):
+
+    #Initalize Table & Variables
+    rows, cols = (9, 8)
+    crateArray = [[0 for i in range(cols)] for j in range(rows)]
+    lineNum = int(0)
+    charNum = int(1)
+    colNum = int(0)
+
+    #Open file, check every 4th character (actual letters)
+    with open(os.environ['USERPROFILE'] + '/Downloads/Day5Table.txt') as tableFile:
+        for line in tableFile.readlines():
+            while charNum < len(line):
+                if not (line[charNum].isspace()): #if not space then add the character to 2D array
+                    crateArray[colNum][lineNum] = line[charNum]
+                    charNum += 4
+                else:
+                    charNum += 4
+                colNum += 1
+
+            #Reset variables to stay inside array indexes and move to next line in 2D array
+            colNum = int(0)
+            lineNum += 1
+            charNum = 1
+        return crateArray
+
+def smarterArray():
+    #Initialize variables
+    arr = [""   ] * 9
+    charNum = int(1)
+    colNum = int(0)
+
+    with open(os.environ['USERPROFILE'] + '/Downloads/Day5Table.txt') as tableFile:
+        for line in tableFile.readlines():
+
+            while charNum < len(line):
+
+                if not (line[charNum].isspace()):
+                    arr[colNum] = arr[colNum] + (line[charNum])
+                    charNum += 4
+                else:
+                    charNum += 4
+                colNum += 1
+
+            #Reset variables to stay inside array indexes and move to next line in array
+            colNum = int(0)
+            charNum = 1
+        return arr
+
+
+
+with open(os.environ['USERPROFILE'] + '/Downloads/Day5.txt') as file:
+    crateArray = smarterArray()
+    #print(crateArray)
+    for line in file.readlines():
+
+        #gather instructions
+        moveAmount = int((line[5] + line[6]).strip())
+        fromCol = int((line[12] + line[13]).strip()) - 1
+        toCol = int((line[17] + line[18]).strip()) - 1 #whitespace added to end of file otherwise error on index 18
+
+        #perform instructions one at a time(KEYWORD: one at a time)
+        i = 1
+        while i <= moveAmount:
+            toMove = crateArray[fromCol][0]
+            crateArray[fromCol] = crateArray[fromCol][1:]
+            crateArray[toCol] = toMove + crateArray[toCol]
+            i += 1
+
+    #When all is performed, report results
+    print(crateArray)
+    for col in crateArray:
+        print(col[0])
